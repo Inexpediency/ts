2fc8be05ac63fc91fe5741a017ffd64138992edb
ythosa <vasus714@yandex.ru>
2020-06-27 11:08:12 +0300

    init

---
+++ 1_types.js
@@ -0,0 +1,5 @@
+"use strict";
+var str = 'Hello';
+var isFetching = true;
+var int = 42;
+console.log(str);
---
+++ 1_types.ts
@@ -0,0 +1,5 @@
+const str: string = 'Hello'
+const isFetching: boolean = true
+const int: number = 42
+
+console.log(str)
