4dbe69d4e664088faaf1d07854751a603d2d343f
ythosa <vasus714@yandex.ru>
2020-06-27 11:20:29 +0300

    base types

--- 1_types.js
+++ 1_types.js
@@ -1,5 +1,16 @@
 "use strict";
-var str = 'Hello';
+var message = 'Hello';
 var isFetching = true;
-var int = 42;
-console.log(str);
+var int = 3e10;
+var arr = [1, 1, 2, 3, 5];
+var arr1 = [1, 1, 2, 3, 5];
+// Tuple
+var person = ['Ruslan', 16];
+// Any
+var variable = 42;
+variable = 'any';
+// Functions
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Ruslan');
--- 1_types.ts
+++ 1_types.ts
@@ -1,5 +1,36 @@
-const str: string = 'Hello'
+const message: string = 'Hello'
 const isFetching: boolean = true
-const int: number = 42
+const int: number = 3e10
 
-console.log(str)
+const arr: Array<number> = [1, 1, 2, 3, 5]
+const arr1: number[] = [1, 1, 2, 3, 5]
+
+// Tuple
+const person: [string, number] = ['Ruslan', 16]
+
+// Any
+let variable: any = 42
+variable = 'any'
+
+// Functions
+function sayMyName(name: string): void {
+    console.log(name)
+}
+
+// Never
+function throwError(message: string): never {
+    throw new Error(message)
+}
+function infinite(): never {
+    while (true) {}
+}
+
+// Custom Types
+type Login = string
+const login: Login = 'admin'
+
+type ID = string | number
+const id1: ID = 12
+const id2: ID = '12'
+
+type SomeType = string | null | undefined
